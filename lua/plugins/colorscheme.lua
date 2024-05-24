55da1afad643d7b046de02f330c5b97d140ee1f5
Laurenz Guevara <laurenzguevara@outlook.com>
2024-05-24 01:26:22 +0100

    revert colorscheme
    Carbon doesn't play nice with lazygit

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,26 +1,25 @@
-return {
-  { "nyoom-engineering/oxocarbon.nvim" },
-
-  {
-    "LazyVim/LazyVim",
-
-    opts = {
-      colorscheme = "oxocarbon",
-    },
-  },
-}
-
 -- return {
---   {
---     "catppuccin",
---     opts = {
---       transparent_background = false,
---     },
---   },
+--   { "nyoom-engineering/oxocarbon.nvim" },
+--
 --   {
 --     "LazyVim/LazyVim",
 --     opts = {
---       colorscheme = "catppuccin",
+--       colorscheme = "oxocarbon",
 --     },
 --   },
 -- }
+
+return {
+  {
+    "catppuccin",
+    opts = {
+      transparent_background = false,
+    },
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin",
+    },
+  },
+}
