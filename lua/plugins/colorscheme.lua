d50d76823a82be0c60b878cb5a6ed69cb9063df9
Laurenz Guevara <laurenzguevara@outlook.com>
2024-05-24 01:25:21 +0100

    more additions

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,26 @@
+return {
+  { "nyoom-engineering/oxocarbon.nvim" },
+
+  {
+    "LazyVim/LazyVim",
+
+    opts = {
+      colorscheme = "oxocarbon",
+    },
+  },
+}
+
+-- return {
+--   {
+--     "catppuccin",
+--     opts = {
+--       transparent_background = false,
+--     },
+--   },
+--   {
+--     "LazyVim/LazyVim",
+--     opts = {
+--       colorscheme = "catppuccin",
+--     },
+--   },
+-- }
